bf6b24cb6d6dec8fd77d1e0630c2dcf6ca89f20c
胡昊泽 <2775194510@qq.com>
2023-04-13 15:48:10 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+// .vuepress/config.js
+
+module.exports = {
+    theme: 'reco',
+    title: '小泽博客正在开发',
+    markdown: {
+      lineNumbers: true,
+    },
+    themeConfig: {
+      //导航
+      nav: [{ text: "首页", link: "/" }],
+      // 博客配置
+      blogConfig: {
+        category: {
+          location: 2, // 在导航栏菜单中所占的位置，默认2
+          text: "博客", // 默认文案 “分类”
+        },
+        tag: {
+          location: 3, // 在导航栏菜单中所占的位置，默认3
+          text: "Tag", // 默认文案 “标签”
+        },
+      },
+    },
+}  
