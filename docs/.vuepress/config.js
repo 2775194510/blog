9c63fc1530176f707ecc44864dc624250a0e10d8
胡昊泽 <2775194510@qq.com>
2023-04-13 17:10:25 +0800

    修改项目环境

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,22 @@
 // .vuepress/config.js
-
 module.exports = {
-    base: "./",
+    base: "/xiaoze-blog/",
     theme: 'reco',
-    title: '小泽博客正在开发',
+    title: '小泽认真奋进',
     markdown: {
       lineNumbers: true,
     },
     themeConfig: {
+      type: 'blog',
+      logo: '/logo.png',
+      authorAvatar: '/logo.png',
       //导航
-      nav: [{ text: "首页", link: "/" }],
+      nav: [{ text: "首页", link: "/" },{ text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }],
       // 博客配置
       blogConfig: {
         category: {
           location: 2, // 在导航栏菜单中所占的位置，默认2
-          text: "博客", // 默认文案 “分类”
+          text: "菜单控制", // 默认文案 “分类”
         },
         tag: {
           location: 3, // 在导航栏菜单中所占的位置，默认3
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-markdown语法正在开发中....
+---
+home: true
+heroImage: /logo.png
+bgImage: '/aboutUs.jpg'
+heroText: 学习永无止境，人生永无上限！
+heroImageStyle: {
+  maxHeight: '200px',
+  display: block,
+  margin: '6rem auto 1.5rem',
+  borderRadius: '50%',
+  boxShadow: '0 5px 18px rgba(0,0,0,0.2)'
+}
+---
